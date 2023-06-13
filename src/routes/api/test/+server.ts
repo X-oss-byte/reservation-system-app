a6fec63a4502af9a45adef53a614c5ce41bdaa40
Matúš Ferčák <74977647+MatusFercak@users.noreply.github.com>
2023-06-14 01:02:25 +0200

    build Bug Patch

--- src/routes/api/test/+server.ts
+++ src/routes/api/test/+server.ts
@@ -1,27 +1,12 @@
-import { API_KEY, API_KEY_2 } from '$env/static/private';
+import { API_KEY } from '$env/static/private';
 
 export const GET = () => {
-	console.log(API_KEY_2);
-
-	const sdk = require('node-appwrite');
-	// Init SDK
-	const client = new sdk.Client();
-
-	const databases = new sdk.Databases(client);
-
-	client
-		.setEndpoint('https://appwrite.bespeak.site/v1') // Your API Endpoint
-		.setProject('bespeak') // Your project ID
-		.setKey('API_KEY'); // Your secret API key
-
-	const promise = databases.list();
-
 	promise.then(
 		function (response) {
-			return new Response(JSON.stringify({ api: API_KEY, res: response }), { status: 200 });
+			return new Response(JSON.stringify({ api: 332, res: response }), { status: 200 });
 		},
 		function (error) {
-			return new Response(JSON.stringify({ api: API_KEY, res: error }), { status: 200 });
+			return new Response(JSON.stringify({ api: 333, res: error }), { status: 200 });
 		}
 	);
 };

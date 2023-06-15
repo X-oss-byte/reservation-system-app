ee6fddb9a001c0d547ef85765ec2b475e46b360f
Matúš Ferčák <74977647+MatusFercak@users.noreply.github.com>
2023-06-15 07:52:16 +0200

    patch

--- src/lib/components/app/HeroSection.svelte
+++ src/lib/components/app/HeroSection.svelte
@@ -7,25 +7,12 @@
 			<p class="max-w-2xl mx-auto" />
 			<div class="items-center justify-center gap-x-3 space-y-3 sm:flex sm:space-y-0">
 				<a
-					href="/"
+					href="/login"
 					class="block py-2 px-4 text-white font-medium bg-gray-900 duration-150 hover:bg-gray-500 active:bg-gray-600 rounded-lg shadow-lg hover:shadow-none"
 				>
 					Get started with Us
 				</a>
-				<!-- <a
-					href="/"
-					class="block py-2 px-4 text-gray-700 hover:text-gray-500 font-medium duration-150 active:bg-gray-100 border rounded-lg"
-				>
-					Možnosti
-				</a> -->
 			</div>
 		</div>
-		<!-- <div class="mt-14">
-			<img
-				src="https://res.cloudinary.com/floatui/image/upload/v1670150563/desktop_dte2ar.png"
-				class="w-full shadow-lg rounded-lg border"
-				alt=""
-			/>
-		</div> -->
 	</div>
 </section>

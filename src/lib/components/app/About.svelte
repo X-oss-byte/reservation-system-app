50334a17e17d40758ba91b818988d7ad8c7dc36a
Matúš Ferčák <74977647+MatusFercak@users.noreply.github.com>
2023-06-15 08:36:27 +0200

    patch

--- src/lib/components/app/About.svelte
+++ src/lib/components/app/About.svelte
@@ -1,9 +1,16 @@
 <section>
-	<div class=" max-w-screen-xl mx-auto px-4 pt-14 pb-28 gap-12 text-gray-600 md:px-8">
+	<div class=" max-w-screen-xl mx-auto px-4 pt-14 pb-10 gap-12 text-gray-600 md:px-8">
 		<div class="space-y-5 max-w-4xl mx-auto text-center">
 			<h2 class="text-4xl text-gray-800 font-extrabold mx-auto md:text-5xl">
 				<span>About page.</span>
 			</h2>
+			<h2 class="text-xl text-gray-800 font-bold">
+				Reservations help businesses optimize their operations by effectively managing their
+				resources and providing better customer service.
+			</h2>
 		</div>
+	</div>
+	<div class="w-full sm:h-screen sm:mt-10 p-5 rounded-xl flex items-center justify-center">
+		<img class="object-center rounded-2xl" src="/img.png" alt="" />
 	</div>
 </section>
